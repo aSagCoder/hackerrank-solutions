8c8024fae9938622de9b6e70247d094ddd4c620d
Kuhu Bhatnagar <89660722+aSagCoder@users.noreply.github.com>
2023-07-19 17:26:54 +0530

    Create runnerup.py

---
+++ Python/Subdomains/Basic Data Types/Find the Runner-Up Score!/runnerup.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    unique_scores = sorted(set(arr), reverse=True)
+    
+    if len(unique_scores) > 1:
+        runner_up_score = unique_scores[1]
+        print(runner_up_score)
